4dad6e77c2bd2e1d5954b786bbced9b17f8a78d6
Jonathan Pritchard <jon@node28.com>
2020-03-04 11:32:21 +0000

    Added DTOs

---
+++ src/dto/ArrivalLog.ts
@@ -0,0 +1,15 @@
+import Captain from './Captain';
+import Vessel from "./Vessel";
+import Port from "./Port";
+
+export default class ArrivalLog {
+  readonly captain: Captain;
+  readonly vessel: Vessel;
+  readonly port: Port;
+  readonly timestamp: Date;
+  constructor(captain: Captain, vessel: Vessel, port: Port) {
+    this.captain = captain;
+    this.vessel = vessel;
+    this.port = port;
+  }
+}
---
+++ src/dto/Vessel.ts
@@ -0,0 +1,6 @@
+export default class Vessel {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+}
